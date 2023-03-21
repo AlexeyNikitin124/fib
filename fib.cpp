a32043ce84b17d65272d6594f10ad03ef41b7787
AlexeyNikitin124 <127249788+AlexeyNikitin124@users.noreply.github.com>
2023-03-22 00:03:33 +0400

    fib(1,1,...)

--- fib.cpp
+++ fib.cpp
@@ -2,8 +2,14 @@
 
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello, World!";
+long long int  fib(long long  n){
+    if (n == 0) return 1;
+    if (n == 1 || n == 2) return 1;
+    return fib(n-1) + fib(n-2);
+}
+int main(){
+    int n;
+    cout << "Введите n: "; cin >> n;
+    cout << fib(n);
     return 0;
 }
