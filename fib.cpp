60e558fcf3a15862064087b60c8ccb250129d24e
AlexeyNikitin124 <127249788+AlexeyNikitin124@users.noreply.github.com>
2023-03-22 00:05:17 +0400

    fib(0,1,...)

--- fib.cpp
+++ fib.cpp
@@ -2,12 +2,12 @@
 
 using namespace std;
 
-long long int  fib(long long  n){
-    if (n == 0) return 1;
-    if (n == 1 || n == 2) return 1;
-    return fib(n-1) + fib(n-2);
+long long int  fib(long long  n) {
+    if (n == 1 || n == 0) return 0;
+    if (n == 2) return 1;
+    return fib(n - 1) + fib(n - 2);
 }
-int main(){
+int main() {
     int n;
     cout << "Введите n: "; cin >> n;
     cout << fib(n);
